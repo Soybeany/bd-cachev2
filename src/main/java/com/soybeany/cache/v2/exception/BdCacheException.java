f35fc6ac04b7f79b509697fad330603cfdace2fb
Soybeany <soybeany@qq.com>
2022-03-30 08:44:03 +0800

    使用统一的bd-exception

--- src/main/java/com/soybeany/cache/v2/exception/BdCacheException.java
+++ src/main/java/com/soybeany/cache/v2/exception/BdCacheException.java
@@ -1,10 +1,12 @@
 package com.soybeany.cache.v2.exception;
 
+import com.soybeany.exception.BdException;
+
 /**
  * @author Soybeany
  * @date 2022/2/8
  */
-public class BdCacheException extends Exception {
+public class BdCacheException extends BdException {
     public BdCacheException(String message) {
         super(message);
     }
--- src/main/java/com/soybeany/cache/v2/exception/BdCacheRtException.java
+++ src/main/java/com/soybeany/cache/v2/exception/BdCacheRtException.java
@@ -1,10 +1,12 @@
 package com.soybeany.cache.v2.exception;
 
+import com.soybeany.exception.BdRtException;
+
 /**
  * @author Soybeany
  * @date 2022/2/8
  */
-public class BdCacheRtException extends RuntimeException {
+public class BdCacheRtException extends BdRtException {
     public BdCacheRtException(String message) {
         super(message);
     }
